0be8219a36a935dd2b77be207b2564eb920de732
renatao <paulo05renato@gmail.com>
2023-11-11 09:28:59 -0300

    add fixtures for login customer and manager pages

--- test/conftest.py
+++ test/conftest.py
@@ -1,6 +1,8 @@
 import pytest
 
+from pages.CustomerPage import CustomerPage
 from pages.LoginPage import LoginPage
+from pages.ManagerPage import ManagerPage
 
 
 @pytest.fixture
@@ -11,3 +13,25 @@ def login_page():
     yield login_page
 
     login_page.close()
+
+
+@pytest.fixture
+def customer_page(login_page):
+    login_page.make_login_customer()
+
+    customer_page = CustomerPage(login_page.driver)
+
+    yield customer_page
+
+    customer_page.close()
+
+
+@pytest.fixture
+def manager_page(login_page):
+    login_page.make_login_manager()
+
+    manager_page = ManagerPage(login_page.driver)
+
+    yield manager_page
+
+    manager_page.close()
