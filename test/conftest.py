6f8ee61894811a38543adf2465d6aad2e573b2f4
renatao <paulo05renato@gmail.com>
2023-11-10 23:13:56 -0300

    refactor function login

--- test/conftest.py
+++ test/conftest.py
@@ -1,34 +1,13 @@
 import pytest
 
-from tests.pages.LoginPage import LoginPage
-
-
-def pytest_addoption(parser):
-    parser.addoption("--browser", default="chrome", help="Ajuda aí jovem, passa o browser válido")
+from pages.LoginPage import LoginPage
 
 
 @pytest.fixture
-def login_page(request):
-    select_browser = request.config.getoption("--browser")
-    login_page = LoginPage(browser=select_browser)
+def login_page():
+    login_page = LoginPage()
     login_page.open()
 
     yield login_page
 
     login_page.close()
-
-
-@pytest.fixture()
-def run_all_browser(all_browsers):
-    login_page = LoginPage(browser=all_browsers)
-    login_page.open()
-
-    yield login_page
-
-    login_page.close()
-
-
-@pytest.fixture
-def login_saucedemo(login_page):
-    login_page.make_login()
-    yield login_page
