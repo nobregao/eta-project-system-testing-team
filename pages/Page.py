9caa09a640f632c51a1388ee3ba1d5f1182f45c9
renatao <paulo05renato@gmail.com>
2023-11-10 23:13:39 -0300

    add function get_element

--- pages/Page.py
+++ pages/Page.py
@@ -1,21 +1,28 @@
 from selenium import webdriver
+from selenium.common import TimeoutException
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 class Page:
 
-    possible_browsers = {
-        "chrome": webdriver.Chrome
-    }
-
-    def __init__(self, url="", driver=None, browser=None):
+    def __init__(self, url="", driver=None):
         self.url = url
         if driver is not None:
             self.driver = driver
         else:
-            self.driver = self.possible_browsers["chrome"]()
+            self.driver = webdriver.Chrome()
 
     def open(self):
         self.driver.get(self.url)
+
+    def get_element(self, by, value, timeout=1):
+        try:
+            element = WebDriverWait(self.driver, timeout).until(
+                expected_conditions.visibility_of_element_located((by, value)))
+        except TimeoutException:
+            return False
+        return element
 
     def close(self):
         self.driver.quit()
