539a4b7943b2cf8e253ad69b7366fea591a22d02
renatao <paulo05renato@gmail.com>
2023-11-11 09:29:26 -0300

    add functions for click in buttons at ManagerPage

--- pages/ManagerPage.py
+++ pages/ManagerPage.py
@@ -1,3 +1,5 @@
+from selenium.webdriver.common.by import By
+
 from pages.Page import Page
 
 
@@ -7,4 +9,16 @@ class ManagerPage(Page):
 
     def __init__(self, driver):
         super().__init__(self.url, driver)
+
+    def click_aba_add_customer(self):
+        button = self.get_element(By.CSS_SELECTOR, "[ng-class='btnClass1']")
+        button.click()
+
+    def click_aba_open_account(self):
+        button = self.get_element(By.CSS_SELECTOR, "[ng-class='btnClass2']")
+        button.click()
+
+    def click_aba_customers(self):
+        button = self.get_element(By.CSS_SELECTOR, "[ng-class='btnClass3']")
+        button.click()
 
